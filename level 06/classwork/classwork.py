0be0321d6144ea5f2d6d295ab8b2b65b6c15cf70
DavitAdeishvili <davitadeishvili31@gmail.com>
2024-06-09 18:33:57 +0400

    seventh commit

---
+++ level 06/classwork/classwork.py
@@ -0,0 +1,18 @@
+#print("hello world")
+
+#dog_name2 = "John"
+#dog_name2 = "Max"
+#DogName3 = 'Husky'
+
+#print("First dog's name is" + " " + dog_name1 + ". " + "Second dog's name is" + " " + dog_name2 + ", " "third dog's name is" + " " + DogName3 + "." )
+#print (f"First dog's name is {dog_name1}")
+
+# I used snake case, camel case, comment and f string.
+
+
+
+
+num1=input("enter your age")
+num2= input("enter second number")
+
+print (num1 + int (num2))
