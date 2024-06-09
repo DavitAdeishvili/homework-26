3ef0063cc6906dd9363d30139acb960afeb5f2ad
DavitAdeishvili <davitadeishvili31@gmail.com>
2024-06-09 18:02:29 +0400

    will be edited

--- level 02./homework.py
+++
@@ -1,1 +0,0 @@
-print ("Gabriel Jobava" ", " "Mariami Gogia" ", " "Giorgi Kupatadze" ", " "Davit Narimanidze" ", " "Giorgi Lalashvili" ", " "Saba Gorgadze")
--- level 03./classwork.py
+++
@@ -1,6 +0,0 @@
-print ("hello world")
-
-
-int1=20
-int2=24
-print(str(int1)+str(int2))
